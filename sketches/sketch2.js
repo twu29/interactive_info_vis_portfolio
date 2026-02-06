2dbdb01193a0e6c90839c6588eace29e4dcc0fca
twu29 <twu29@uw.edu>
2026-02-05 22:16:50 -0800

    added basic clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -3,12 +3,28 @@ registerSketch('sk2', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
+  
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.background(250, 248, 245);
+    
+    // Draw clock circle
+    p.push();
+    p.translate(p.width / 2, p.height / 2);
+    p.noFill();
+    p.stroke(30);
+    p.strokeWeight(3);
+    p.circle(0, 0, 280);
+    
+    // Draw center dot
+    p.fill(30);
+    p.noStroke();
+    p.circle(0, 0, 8);
+    
+    p.pop();
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+  
+  p.windowResized = function () { 
+    p.resizeCanvas(p.windowWidth, p.windowHeight); 
+  };
 });
+
