5f42e7f6684cda9501597305b8f4e16ef175a242
twu29 <twu29@uw.edu>
2026-02-05 22:23:53 -0800

    added hour min hands and numbers on clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -15,10 +15,47 @@ registerSketch('sk2', function (p) {
     p.strokeWeight(3);
     p.circle(0, 0, 280);
     
+    // Draw clock numbers
+    p.fill(30);
+    p.noStroke();
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(20);
+    for (let i = 1; i <= 12; i++) {
+      let angle = p.map(i, 0, 12, -p.PI / 2, 3 * p.PI / 2);
+      let x = p.cos(angle) * 120;
+      let y = p.sin(angle) * 120;
+      p.text(i, x, y);
+    }
+    
     // Draw center dot
     p.fill(30);
     p.noStroke();
     p.circle(0, 0, 8);
+    
+    // Get current time
+    let h = p.hour() % 12;
+    let m = p.minute();
+    let s = p.second();
+    
+    // Calculate angles (in radians, -PI/2 to start at top)
+    let hourAngle = p.map(h + m / 60, 0, 12, -p.PI / 2, 3 * p.PI / 2);
+    let minuteAngle = p.map(m + s / 60, 0, 60, -p.PI / 2, 3 * p.PI / 2);
+    
+    // Draw minute hand
+    p.push();
+    p.rotate(minuteAngle);
+    p.stroke(60);
+    p.strokeWeight(4);
+    p.line(0, 0, 80, 0);
+    p.pop();
+    
+    // Draw hour hand
+    p.push();
+    p.rotate(hourAngle);
+    p.stroke(60);
+    p.strokeWeight(6);
+    p.line(0, 0, 40, 0);
+    p.pop();
     
     p.pop();
   };
@@ -27,4 +64,3 @@ registerSketch('sk2', function (p) {
     p.resizeCanvas(p.windowWidth, p.windowHeight); 
   };
 });
-
